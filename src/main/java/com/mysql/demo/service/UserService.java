0bfe48979af51db5eee3545193353c070d122ee1
Prashant lokhande <lokhande.prashant40@gmail.com>
2023-01-16 12:18:11 +0530

    service method added

--- src/main/java/com/mysql/demo/service/UserService.java
+++ src/main/java/com/mysql/demo/service/UserService.java
@@ -1,5 +1,10 @@
 package com.mysql.demo.service;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.mysql.demo.model.User;
+
 public interface UserService {
+	
+	public JSONPObject saveUser(User user);
 
 }
